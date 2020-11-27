ca3998322ff2e3c5a3c9f4b9bd384463a6053072
DimitrenkoTaras <dimitrenkotaras@gmail.com>
2020-11-27 13:48:09 +0200

    version-final

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header-menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.header-menu__list').click(function(event) {
+        $('.header__burger, .header-menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
+    
+    $('.community-slider').slick({
+        slidesToShow:2,
+        dots:true,
+        arrows:false,
+        autoplay:true,
+        speed: 1000,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:1,
+                }
+            }
+        ]
+    });
+});
+
+const animItems = document.querySelectorAll('.animation');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('active');
+            } else {
+                if (!animItem.classList.contains('animation-no-repeat')) {
+                    animItem.classList.remove('active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
